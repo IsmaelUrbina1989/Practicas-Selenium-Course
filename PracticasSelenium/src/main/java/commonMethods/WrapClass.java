d1c05bb890efc68812566bbbb66084cba6910f99
user <user@DESKTOP-ISMAEL>
2024-02-22 19:44:14 -0600

    commit

--- PracticasSelenium/src/main/java/commonMethods/WrapClass.java
+++ PracticasSelenium/src/main/java/commonMethods/WrapClass.java
@@ -28,14 +28,11 @@ public class WrapClass {
 	}
 
 	public static void takeScreenshot (WebDriver webDriver, String testCaseName) {
-		//Toman el screenshot de la pagina sin formato
 		File scrFile = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
-
-		//Establece ruta donde se guarda el screenshot
 		String screenshotPath = "./test-output/ExecutionResults";
-
+		
 		try {
-			FileHandler.createDir(new File (screenshotPath));//Crea el folder usando la ruta especifica de la variable "screenshotPath"
+			FileHandler.createDir(new File (screenshotPath));
 			FileHandler.copy(scrFile, new File(screenshotPath + File.separatorChar + testCaseName + ".png"));
 		}catch (IOException e) {
 			e.printStackTrace();
